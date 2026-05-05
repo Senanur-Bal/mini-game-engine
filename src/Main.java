9b0ebb9cf1e99bb464246a174b78dca613c2dc8f
Senanur Bal <senanurbal38@gmail.com>
2026-05-05 13:31:07 +0300

    feat: complete Factory Method refactor with polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        GameObject obj1 = new GameObject("player", 10, 20);
-        GameObject obj2 = new GameObject("enemy", 5, 15);
-        GameObject obj3 = new GameObject("item", 8, 8);
 
-        obj1.update();
-        obj2.update();
-        obj3.update();
+        GameObject player = GameObjectFactory.create("player", 10, 20);
+        GameObject enemy = GameObjectFactory.create("enemy", 5, 15);
+        GameObject item = GameObjectFactory.create("item", 8, 8);
+
+        player.update();
+        enemy.update();
+        item.update();
     }
 }
